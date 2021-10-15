f5ead998bc35ba6bd8d1d53c3282ae7d16054748
lvivas <vivasleonardo4@gmail.com>
2021-10-15 13:27:34 -0300

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@ window.addEventListener('scroll', function () {
 
     function fade(direction){
 
-        let imagen = document.querySelectorAll('.img__' + direction);
+        let imagen = document.querySelectorAll('.img__' + direction)
 
     for (let i = 0; i < imagen.length; i++) {
 
-        let altura = window.innerHeight / 1.3;
+        let altura = window.innerHeight / 1.3
 
-        let distancia = imagen[i].getBoundingClientRect().top;
+        let distancia = imagen[i].getBoundingClientRect().top
 
 
         console.log(altura)
         console.log(distancia)
 
-        imagen[i].classList.add('transform_'+direction)
+        imagen[i].classList.add('transform_' + direction)
 
 
         if (distancia <= altura) {
 
-            imagen[i].classList.add('img__aparece');
+            imagen[i].classList.add('img__aparece')
         }
         else {
 
-            imagen[i].classList.remove('img__aparece');
+            imagen[i].classList.remove('img__aparece')
         }
 
 
